79ffff6cbfdd1f3aefa94669d0418cee5676c0e3
BrunodevBandeira <brunodev29ab@gmail.com>
2022-12-16 16:24:49 -0300

    Add clone Netflix

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,20 +1,33 @@
 export default function initTooltip() {
 
-	const tooltips = document.querySelectorAll("[data-tooltip]");
+	// const tooltips = document.querySelectorAll("[data-tooltip]");
 
-	tooltips.forEach((item) => {
-		item.addEventListener("mouseover", onmouseover);
-	});
+	// tooltips.forEach((item) => {
+	// 	item.addEventListener("mouseover", onmouseover);
+	// });
 
-	function onmouseover(event) {
-		criarToolTipBox(this);
-		console.log(event)
-	};
+	// function onmouseover(event) {
+	// 	const tooltipBox = criarToolTipBox(this);
+	// 	tooltipBox.style.top = event.pageY + "px";
+	// 	tooltipBox.style.top = event.pageX + "px";
 
-	function criarToolTipBox(element) {
-		const tooltipBox = document.createElement("div");
-		const text = element.getAttribute("aria-label");
-		tooltipBox.classList.add("tooltip");
-		tooltipBox.innerText = text;
-	}
+	// 	onMouseLeave.pegaTooltipBox = tooltipBox;
+	// 	this.addEventListener("mouseleave", onMouseLeave);
+	// }
+
+	// const onMouseLeave = {
+	// 	pegaTooltipBox: "",
+	// 	handleEvent() {
+	// 		this.pegaTooltipBox.remove();
+	// 	}
+	// }
+
+	// function criarToolTipBox(element) {
+	// 	const tooltipBox = document.createElement("div");
+	// 	const text = element.getAttribute("aria-label");
+	// 	tooltipBox.classList.add("tooltip");
+	// 	tooltipBox.innerText = text;
+	// 	document.body.appendChild(tooltipBox);
+	// 	return tooltipBox;
+	// }
 }
