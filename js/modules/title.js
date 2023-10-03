9f0e5b3022d339a4f2e3ffc4025dd351e0b55486
Bruno Bandeira <brunodev29ab@gmail.com>
2023-10-03 10:31:32 -0300

    correções

--- js/modules/title.js
+++ js/modules/title.js
@@ -1,5 +1,5 @@
 export default function initTitle() {
     const date = new Date();
 // console.log(date.getMinutes());
-date.getMinutes() % 2 === 0 ? document.title = "Bruno Bandeira 2022" : document.title = "Seja Bem vindo...";
+date.getMinutes() % 2 === 0 ? document.title = "Bruno Bandeira 2023" : document.title = "Seja Bem vindo...";
 }
